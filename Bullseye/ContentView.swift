b416d8545f7ea1288347298a58d1b5e982ac0a29
Jayanth Manklu <jayanthms7@gmail.com>
2020-04-04 23:43:35 +0530

    Am I building an iOS app? I am building an iOS app!

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
+    @State var whoseThereIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                Text("Welcome to my first app!")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                Button(action: {
+                    print("Button Pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) {
+                    return Alert(title: Text("Hello there!"), message: Text("This is my first Pop-up"), dismissButton: .default(Text("Awesome!")))
+                    
+                }
+            }
+            Button(action: {
+                self.whoseThereIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Knock Knock"/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.red)
+            }
+            .alert(isPresented: $whoseThereIsVisible) {
+                return Alert(title: Text("Who's There?"), message: Text("This is my knock-knock joke"), dismissButton: .default(Text("Get Out!")))
+                
+            }
+        }
     }
 }
 
