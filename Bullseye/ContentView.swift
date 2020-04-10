370043edf23e774d4752bf62df6754b8d2065126
Jayanth Manklu <jayanthms7@gmail.com>
2020-04-10 18:06:59 +0530

    Bullseye all functionality complete!
    - Have a slider (1 to 100) to get the value from user
    - Randomly generate a number to compare with slider value when they tap "Hit Me"
    - 100 minus absolute difference between slider value and random target value is score saved to state variable
    - A state variable stores cumulative scores from successful games
    - One more state variable keep track of how many games you have played
    - Used HStack, VStack and some Spacers to make it look like a game, bare bones as of now
    - forced landscape mode in the app

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,43 +9,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var alertIsVisible: Bool = false
-    @State var whoseThereIsVisible: Bool = false
+    
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
     
     var body: some View {
         VStack {
-            VStack {
-                Text("Welcome to my first app!")
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.green)
-                Button(action: {
-                    print("Button Pressed!")
-                    self.alertIsVisible = true
-                }) {
-                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-                }
-                .alert(isPresented: $alertIsVisible) {
-                    return Alert(title: Text("Hello there!"), message: Text("This is my first Pop-up"), dismissButton: .default(Text("Awesome!")))
-                    
-                }
+            Spacer()
+            // Target row
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("\(target)")
             }
+            Spacer()
+            // Slider row
+            HStack {
+                Text("1")
+                Slider(value: $sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+            // Button row
             Button(action: {
-                self.whoseThereIsVisible = true
+                self.alertIsVisible = true
             }) {
-                Text(/*@START_MENU_TOKEN@*/"Knock Knock"/*@END_MENU_TOKEN@*/)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.red)
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
             }
-            .alert(isPresented: $whoseThereIsVisible) {
-                return Alert(title: Text("Who's There?"), message: Text("This is my knock-knock joke"), dismissButton: .default(Text("Get Out!")))
-                
+            .alert(isPresented: $alertIsVisible) {
+                return Alert(title: Text("Hello there!"), message: Text(
+                    "The Slider's value is \(sliderValueRounded()).\n" +
+                    "You scored \(pointsForCurrentRound()) points this round."
+                    ), dismissButton: .default(Text("Awesome!")) {
+                        self.score += self.pointsForCurrentRound()
+                        self.target = Int.random(in: 1...100)
+                        self.round += 1
+                    })
             }
+            Spacer()
+            // Score row
+            HStack {
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("\(score)")
+                Spacer()
+                Text("Round:")
+                Text("\(round)")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
         }
+    }
+    
+    func pointsForCurrentRound() -> Int {
+        100 - abs(sliderValueRounded() - target)
+    }
+    
+    func sliderValueRounded() -> Int {
+        Int(sliderValue.rounded())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
