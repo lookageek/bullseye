f12a4444e8243faf0256790fb7d51f30448b55dc
Jayanth Manklu <jayanthms7@gmail.com>
2020-04-11 11:46:02 +0530

    Add app icons

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -9,14 +9,35 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        VStack {
-            Text("🎯 Bullseye 🎯")
-            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
-            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score")
-            Text("Enjoy!")
+    let backgroundColor = Color.init(red: 255.0 / 255.0, green: 214.0 / 255.0, blue: 179.0 / 255.0)
+    
+    struct TextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .foregroundColor(Color.black)
+                .padding(.leading, 60)
+                .padding(.trailing, 60)
+                .padding(.bottom, 20)
         }
-    .navigationBarTitle("About Bullseye")
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("🎯 Bullseye 🎯")
+                    .font(Font.custom("Arial Rounded MT Bold", size: 30))
+                    .foregroundColor(Color.black)
+                    .padding(.top, 20)
+                    .padding(.bottom, 20)
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(TextStyle()).lineLimit(nil)
+                Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score").modifier(TextStyle()).lineLimit(nil)
+                Text("Enjoy!").modifier(TextStyle()).lineLimit(nil)
+            }
+            .navigationBarTitle("About Bullseye")
+            .background(backgroundColor)
+        }
+        .background(Image("Background"))
     }
 }
 
--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     @State var target = Int.random(in: 1...100)
     @State var score = 0
     @State var round = 1
-    let midnightBlue = Color(red: 0.0/255.0, green: 51.0/255.0, blue: 102.0/255.0)
+    let midnightBlue = Color(red: 0.0 / 255.0, green: 51.0 / 255.0, blue: 102.0 / 255.0)
     
     struct LabelStyle: ViewModifier {
         func body(content: Content) -> some View {
